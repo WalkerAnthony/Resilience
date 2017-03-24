ad4c15f2f86e09dca26b7c89ad645da9e78a89fa
Colton Horn <horncb14@wfu.edu>
2017-03-24 15:34:40 -0400

    Edited some comments, made code look nicer.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // loading necessary modules
-var express = require('express');
-var httpModule = require('http');
-var bodyParser = require('body-parser');
+var express       = require('express');
+var httpModule    = require('http');
+var bodyParser    = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 var patientTable = "patients";
@@ -38,16 +38,15 @@ app.get('/P_Review', (req, res) => {
   res.sendFile(__dirname + '/P_Review.html');
 });
 
-
-// This will pull from the db and list the patient's
+// This will pull from the db and list the patients
 app.get('/patientlist', function(req, res){
     var patients = 15;
     res.render('patientlist.ejs', {list: patients});
-  });
+});
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/sign_up.html');
-  });
+});
 
 //Respond to POST request for target '/signup'
 app.post('/signup', (req, res) => {
@@ -67,7 +66,7 @@ function portListener() {
 
 var port = process.env.PORT || 3000;
 
-//Below is stuff that Pauca showed us on Wednesday after class.
+//Connect to MongoDB (mlab.com)
 var myDB;
 MongoClient.connect('mongodb://ResGroup:patientsock@ds113630.mlab.com:13630/resiliencedb',
 (err, database) => {
